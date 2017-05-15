54625045d6c808cd68c648d877e0fe579190b430
Amorn Apichattanakul <benamorn@gmail.com>
2017-05-16 06:46:19 +0700

    Initial Commit

---
+++ PlayLabTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayLabTest
+//
+//  Created by Amorn Apichattanakul on 5/16/17.
+//  Copyright © 2017 Amorn Apichattanakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
